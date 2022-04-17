f00ddc5b0bae884908969b844d1d9905c205c742
Jason Barr <jason.barr@metronetinc.com>
2022-04-16 21:03:16 -0500

    First test for CPU

--- src/vm/cpu.test.js
+++ src/vm/cpu.test.js
@@ -1,1 +1,10 @@
+import memory from "./memory.js";
 import CPU from "./cpu.js";
+
+const cpu = new CPU(memory(256));
+
+describe("Initializing a CPU object", () => {
+  it("Creates a new object", () => {
+    expect(typeof cpu).toEqual("object");
+  });
+});

0299303533bfad5fc84b15e6bfaf4b7db3240718
Jason Barr <jason.barr@metronetinc.com>
2022-04-16 21:12:44 -0500

    Create another test

--- src/vm/cpu.test.js
+++ src/vm/cpu.test.js
@@ -7,4 +7,13 @@ describe("Initializing a CPU object", () => {
   it("Creates a new object", () => {
     expect(typeof cpu).toEqual("object");
   });
+
+  it("Stores its memory object for its program and data", () => {
+    expect(cpu.memory instanceof DataView).toBe(true);
+  });
+
+  it("Initializes registers", () => {
+    expect(cpu.registers instanceof DataView).toBe(true);
+    expect(cpu.floatRegisters instanceof DataView).toBe(true);
+  });
 });

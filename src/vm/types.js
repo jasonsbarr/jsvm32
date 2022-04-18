92e55bd176be8d96148a4fe7befda63272f7b8d0
Jason Barr <jason.barr@metronetinc.com>
2022-04-18 13:48:41 -0500

    Not going to use NULL type

--- src/vm/types.js
+++ src/vm/types.js
@@ -2,48 +2,44 @@ import { indexBy } from "../utils.js";
 
 const types = [
   {
-    name: "null",
+    name: "ubyte",
     code: 0x00,
   },
   {
-    name: "ubyte",
+    name: "uword",
     code: 0x01,
   },
   {
-    name: "uword",
+    name: "uint",
     code: 0x02,
   },
   {
-    name: "uint",
+    name: "ulong",
     code: 0x03,
   },
   {
-    name: "ulong",
+    name: "byte",
     code: 0x04,
   },
   {
-    name: "byte",
+    name: "word",
     code: 0x05,
   },
   {
-    name: "word",
+    name: "int",
     code: 0x06,
   },
   {
-    name: "int",
+    name: "long",
     code: 0x07,
   },
   {
-    name: "long",
+    name: "float",
     code: 0x08,
   },
   {
-    name: "float",
-    code: 0x09,
-  },
-  {
     name: "double",
-    code: 0x10,
+    code: 0x09,
   },
 ];
 
